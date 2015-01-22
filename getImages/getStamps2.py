74a77242c2152ac29587ada735234285108e1d65
kristi <kawebb@uvic.ca>
2015-01-22 13:11:52 -0800

    adding more

--- getImages/getStamps2.py
+++ getImages/getStamps2.py
@@ -1,5 +1,5 @@
-"""Retrieval of cutouts of the FITS images associated with the OSSOS detections.
-Takes a directory of .ast file (in dbase format) as input
+"""Retrieval of cutouts of the FITS images associated with the CFHT/MegaCam detections.
+Takes a table (getmbc.py output) as input
 
 An Example URL for cutouts from OSSOS (not CFHT/MegaCam)
 http://www.canfar.phys.uvic.ca/vospace/auth/synctrans?TARGET=vos://cadc.nrc.ca~
@@ -7,16 +7,13 @@ vospace/OSSOS/dbimages/1625356/1625356p.fits&DIRECTION=pullFromVoSpace&PROTOCOL=
 ivo://ivoa.net/vospace/core%23httpget&view=cutout&cutout=CIRCLE+ICRS+242.1318+-1
 2.4747+0.05
 """
-
 # in this case, the image is the exposure number
 
-	
 import argparse
 import logging
 import getpass
 import requests
 import os
-import storage
 
 import numpy as np
 import pandas as pd
@@ -35,29 +32,37 @@ BASEURL = "http://www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/vospace/auth/synctrans"
 		
 def cutout(image, RA, DEC, radius, username, password):
     
-        this_cutout = "CIRCLE ICRS {} {} {}".format(RA, DEC, radius)                                 
-        print this_cutout
+    this_cutout = "CIRCLE ICRS {} {} {}".format(RA, DEC, radius)                                 
+    print this_cutout
+    
+    DBIMAGES = 'vos:OSSOS/dbimages'
+    ext = '.fits'
+    uri = os.path.join(DBIMAGES,'%s%s' % (str(image), ext))
+    print "got uri: " + str(uri)
 
-        target = storage.vospace.fixURI(storage.get_uri(image))
-        direction = "pullFromVoSpace"
-        protocol = "ivo://ivoa.net/vospace/core#httpget"
-        view = "cutout"
-        params = {"TARGET": target,
+
+    target = storage.vospace.fixURI(uri)
+    direction = "pullFromVoSpace"
+    protocol = "ivo://ivoa.net/vospace/core#httpget"
+    view = "cutout"
+    params = {"TARGET": target,
                   "PROTOCOL": protocol,
                   "DIRECTION": direction,
                   "cutout": this_cutout,
                   "view": view}
-        r = requests.get(BASEURL, params=params, auth=(username, password))
-        r.raise_for_status()  # confirm the connection worked as hoped
-        postage_stamp_filename = "{}_{:11.5f}_{:09.5f}_{:+09.5f}.fits".format(obj.provisional_name,
-                                                                              obs.date.mjd,
-                                                                              obs.coordinate.ra.degree,
-                                                                              obs.coordinate.dec.degree)
-        with open(postage_stamp_filename, 'w') as tmp_file:
-            tmp_file.write(r.content)
-            storage.copy(postage_stamp_filename, obj_dir + "/" + postage_stamp_filename)
-        os.unlink(postage_stamp_filename)  # easier not to have them hanging around	
+    r = requests.get(BASEURL, params=params, auth=(username, password))
+    r.raise_for_status()  # confirm the connection worked as hoped
+    postage_stamp_filename = "{:11.5s}_{:11.5f}_{:+11.5f}.fits".format(image, RA, DEC)
+    with open(postage_stamp_filename, 'w') as tmp_file:
+        tmp_file.write(r.content)
+        copy(postage_stamp_filename, obj_dir + "/" + postage_stamp_filename)
+    os.unlink(postage_stamp_filename)  # easier not to have them hanging around	
 
+# from OSSOS storage.py
+def copy(source, dest):
+    """use the vospace service to get a file. """
+    print "copying {} -> {}".format(source, dest)
+    return vospace.copy(source, dest)
 
 # PARSE INFORMATION INPUTTED FROM THE COMMAND LINE
 	# VERSION - OSSOS DATA RELEASE VERSION THE STAMPS ARE TO BE ASSIGNED TO
@@ -71,17 +76,13 @@ def main():
     # IDENTIFY PARAMETERS FOR QUERY OF SSOIS FROM INPUT
 
     parser = argparse.ArgumentParser(
-        description='Parse a directory of TNO .ast files and create links in the postage stamp directory '
-                    'that allow retrieval of cutouts of the FITS images associated with the OSSOS detections. '
+        description='Parse an obects.txt input file (getmbc.py output) and create links in the postage stamp directory '
+                    'that allow retrieval of cutouts of the FITS images associated with the CHT/MegaCam detections. '
                     'Cutouts are defined on the WCS RA/DEC of the object position.')
-
-    parser.add_argument("version",
-                        help="The OSSOS data release version these stamps should be assigned to.")
     parser.add_argument("--ossin",
                         action="store",
                         default="lixImages.txt",
-                        help="The vospace containerNode that clones ossin dbaseclone"
-                             "holding the .ast files of astrometry/photometry measurements.")
+                        help="The input .txt files of astrometry/photometry measurements.")
     parser.add_argument("--radius", '-r',
                         action='store',
                         default=0.02,
@@ -96,17 +97,18 @@ def main():
     
     args = parser.parse_args()
     
-# PRINT HEADER
+    # PRINT HEADER
 
     print "-------------------- \n Cutting postage stamps of objects in input file %s from CFHT/MegaCam images \n--------------------" % args.infile	
-# CADC PERMISSIONS
+
+    # CADC PERMISSIONS
     username = raw_input("CADC username: ")
     password = getpass.getpass("CADC password: ")
 
-# PARSE THROUGH INPUT FILE FOR IMAGE INFORMATION
-    # format into lines, parse for image, RA and DEC
-# CUT OUT IMAGE
-	# PASS: object, directory, radius, CADC permissions                    
+    # PARSE THROUGH INPUT FILE FOR IMAGE INFORMATION
+        # format into lines, parse for image, RA and DEC
+        # CUT OUT IMAGE
+	        # PASS: object, directory, radius, CADC permissions                    
 
     with open(in_file) as infile: 
         for line in infile.readlines()[1:]:
