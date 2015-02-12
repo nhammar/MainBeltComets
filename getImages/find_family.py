d4b3b0572122aa96e523df0f193fcb23e02bdf99
kristi <kawebb@uvic.ca>
2015-02-12 09:53:53 -0800

    Updated case url

--- getImages/find_family.py
+++ getImages/find_family.py
@@ -3,6 +3,7 @@ import requests
 import argparse
 import os
 
+dir_path_base = '/Users/admin/Desktop/MainBeltComets/getImages/asteroid_families'
 
 def main():
 
@@ -32,7 +33,7 @@ def find_family_members(familyname, output=None):
     if output == None:
         output = familyname+'_family.txt'
         
-    dir_path_base = '/Users/admin/Desktop/MainBeltComets/getImages/'
+    
     output_dir = os.path.join(dir_path_base, familyname)
     if os.path.isdir(output_dir) == False:
         os.makedirs(output_dir)
@@ -69,6 +70,41 @@ def find_family_members(familyname, output=None):
               outfile.write("{}\n".format(item))
               
     return asteroid_list
+
+def get_all_families_list(output=None):
+    '''
+    Queries the AstDys database for members of all known families, family name is name of largest member
+    '''
+    
+    if output == None:
+        output = 'all_families.txt'
+    
+
+    BASEURL = 'http://hamilton.dm.unipi.it/~astdys2/propsynth/numb.famtab'
+    
+    r = requests.get(BASEURL)
+    r.raise_for_status()
+    
+    table = r.content
+    table_lines = table.split('\n')
+    
+        
+    print "----- Searching for list of all family names -----"
+        
+    familyname_list = []
+        
+    for line in table_lines[2:len(table_lines)-1]:
+        assert len(line.split()) > 0
+        familyname_list.append(line.split()[0])
+    
+    assert len(familyname_list) > 0
+    print " Number of asteroid families: {}".format(len(familyname_list))            
+    
+    with open('{}/{}'.format(dir_path_base, output), 'w') as outfile:
+        for item in familyname_list:
+              outfile.write("{}\n".format(item))
+              
+    return familyname_list
 
 
 if __name__ == '__main__':
