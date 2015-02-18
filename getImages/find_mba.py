a7c019c127aee2ac4fbf747cb959a01713ef6263
kristi <kawebb@uvic.ca>
2015-02-18 14:31:17 -0800

    Can select start and stop

--- getImages/find_mba.py
+++ getImages/find_mba.py
@@ -2,25 +2,42 @@ import numpy as np
 import requests
 import os
 import pandas as pd
+import argparse
 
 def main():
     
-    find_mbas()
+    parser = argparse.ArgumentParser(
+                        description='Queries the AstDys database for members of the main asteroid belt \
+                        2.064 AU < a < 3.277 AU, e < 0.45, i < 40 deg')
+    parser.add_argument("--start", 
+                        action="store",
+                        default=0,
+                        help="Starting point in asteroid list")
+    parser.add_argument("--stop", 
+                        action='store',
+                        default=100,
+                        help='Stopping point in asteroid list, default is 100')
+                            
+    args = parser.parse_args()
     
-def find_mbas(output=None):    
+    start = int(args.start)
+    stop = int(args.stop)
+    find_mbas(start, stop)
+    
+def find_mbas(start, stop, output=None):    
     '''
     Queries the AstDys database for members of the main asteroid belt
     2.064 AU < a < 3.277 AU, e < 0.45, i < 40 deg, sini < 0.642 
     '''
     
     if output == None:
-        output = 'mba_wofam_list.txt'
+        output = 'all_wofam_list.txt'
     output_dir = '/Users/admin/Desktop/MainBeltComets/getImages/mba'
     
     print "---------- \nSearching for obects of in the Main Asteroid Belt with \n2.064 AU < a < 3.277 AU, e < 0.45, i < 40 deg \n----------"
-        
+    print "----- Searching asteroids with indexes {} - {}".format(start, stop)     
     mba_wofam_list = []
-    with open('mba/mba_wofam_list.txt') as infile:
+    with open('mba/all_wofam_list.txt') as infile:
         for line in infile:
             mba_wofam_list.append(line.strip('\n'))
     
@@ -50,7 +67,7 @@ def find_mbas(output=None):
     sini_list2 = []
     name_list = []
     x = 0
-    for objectname in mba_wofam_list:
+    for objectname in mba_wofam_list[start:stop]:
         index = all_objects_table.query('objectname == "{}"'.format(objectname))
         try:
             if (float(index['semimajor_axis']) > 2.064) & (float(index['eccentricity']) < 0.45) & (float(index['sin_inclination']) < 0.642):
@@ -67,13 +84,13 @@ def find_mbas(output=None):
         
     print " Number of objects found: {}".format(len(name_list))            
     
-    objects_in_mb_table.to_csv('asteroid_families/mba_wo_fam_data.csv', sep='\t', encoding='utf-8')
+    objects_in_mb_table.to_csv('mba/mba_wo_fam_data_{}.txt'.format(start), sep='\t', encoding='utf-8')
     #name_list.to_csv('asteroid_families/mba_fam_list.txt', sep='\t', encoding='utf-8')
     
     #with open('asteroid_families/mba_wo_fam_data.txt', 'w') as outfile:
     #    outfile.write('{}'.format(objects_in_mb_table))
         
-    with open('asteroid_families/mba_fam_list.txt', 'w') as outfile:
+    with open('mba/mba_wo_fam_list_{}.txt'.format(start), 'w') as outfile:
         outfile.write('{}'.format(name_list))
               
     return name_list
