ac7a2b21a6e72707175a2f048613712d8f2b3f2f
kristi <kawebb@uvic.ca>
2015-02-18 11:06:26 -0800

    Reworks with pandas dataframe query

--- getImages/find_mba.py
+++ getImages/find_mba.py
@@ -14,44 +14,62 @@ def find_mbas(output=None):
     
     if output == None:
         output = 'mba_wofam_list.txt'
-        
     output_dir = '/Users/admin/Desktop/MainBeltComets/getImages/mba'
+    
+    print "---------- \nSearching for obects of in the Main Asteroid Belt with \n \
+            2.064 AU < a < 3.277 AU, e < 0.45, i < 40 deg \n----------"
+        
+    mba_wofam_list = []
+    with open('mba/wo_fam_list.txt') as infile:
+        for line in infile:
+            mba_wofam_list.append(line)
     
     BASEURL = 'http://hamilton.dm.unipi.it/~astdys2/propsynth/numb.syn'
     
     r = requests.get(BASEURL)
     r.raise_for_status()
-    
     table = r.content
     table_lines = table.split('\n')
-        
-    print "----- Searching for obects of in the Main Asteroid Belt -----"
-        
-    mba_list = []
-    mba_wofam_list = []
     
-    with open('mba/wo_fam_list.txt') as infile:
-        for line in infile:
-            mba_wofam_list.append(line)
-            
-        
+    tableobject_list = []    
+    a_list = []
+    e_list = []
+    sini_list = []
     for line in table_lines[2:]:
         if len(line.split()) > 0:
-            mba_name = line.split()[0]
-            mba_a = float(line.split()[2])
-            mba_e = float(line.split()[3])
-            mba_i = float(line.split()[4])
-        
-            for item in mba_wofam_list:
-                if (item == mba_name) & (mba_a > 2.064) & (mba_e < 0.45) & (mba_i < 0.6427):
-                    mba_list.append(mba_name)
+            tableobject_list.append(line.split()[0])
+            a_list.append(float(line.split()[2]))
+            e_list.append(float(line.split()[3]))
+            sini_list.append(float(line.split()[4]))
+    table_arrays = {'objectname': tableobject_list, 'semimajor_axis': a_list, 'eccentricity': e_list, 'sin_inclination': sini_list}
+    all_objects_table = pd.DataFrame(data=table_arrays)
+    #print all_objects_table
 
-    #assert len(mba_list) > 0
-    print " Number of objects in the M.A.B.: {}".format(len(mba_list))            
+    a_list2 = []
+    e_list2 = []
+    sini_list2 = []
+    name_list = []
+    for objectname in mba_wofam_list[1:]:
+        try:
+            index = all_objects_table.query('objectname == "{}"'.format(objectname))
+            if (float(index['semimajor_axis']) > 2.064) & (float(index['eccentricity']) < 0.45) & (float(index['sin_inclination']) < 0.642):
+                name_list.append(objectname)
+                a_list2.append(float(index['semimajor_axis']))
+                e_list2.append(float(index['eccentricity']))
+                sini_list2.append(float(index['sin_inclination']))
     
-    with open('{}/{}'.format(output_dir, output), 'w') as outfile:
-        for item in mba_list:
-              outfile.write("{}\n".format(item))
+    table2_arrays = {'objectname': name_list, 'semimajor_axis': a_list2, 'eccentricity': e_list2, 'sin_inclination': sini_list2}
+    objects_in_mb_table = pd.DataFrame(data=table2_arrays)
+
+    print objects_in_mb_table
+        
+    print " Number of objects found: {}".format(len(name_list))            
+    
+    with open('asteroid_families/mba_wo_fam_data.txt', 'w') as outfile:
+        outfile.write('{}'.format(objects_in_mb_table))
+        
+    with open('asteroid_families/mba_fam_list.txt', 'w') as outfile:
+        outfile.write('{}'.format(name_list))
               
     return mba_list
 
