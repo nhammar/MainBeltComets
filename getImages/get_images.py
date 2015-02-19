d7bc540403fef02ce0bcc11d1217877d5827030c
kristi <kawebb@uvic.ca>
2015-02-18 19:49:49 -0800

    Added section for none type families

--- getImages/get_images.py
+++ getImages/get_images.py
@@ -4,9 +4,39 @@ from astropy.io import ascii
 from astropy.time import Time
 import requests
 import argparse
-import requests
-
 from ossos_scripts.ssos import Query
+
+def main():
+    '''
+    Input asteroid family, filter type, and image type to query SSOIS
+    '''
+    
+    parser = argparse.ArgumentParser(description='Run SSOIS and return the available images in a particular filter.')
+
+    parser.add_argument("--filter",
+                    action="store",
+                    default='r',
+                    dest="filter",
+                    choices=['r', 'u'],
+                    help="passband: default is r'")
+    parser.add_argument("--family", '-f',
+                        action="store",
+                        default="testfamily/testfamily_family.txt",
+                        help='list of objects to query')
+    parser.add_argument('--type',
+                        default='p',
+                        choices=['o', 'p', 's'], 
+                        help="restrict type of image (unprocessed, reduced, calibrated)")
+    parser.add_argument('--suffix',
+                        default=None,
+                        help="Suffix of object list file")
+                        
+    args = parser.parse_args()
+    
+    if args.suffix == None:
+        get_image_info(args.family, args.filter, args.type)
+    else:
+        get_image_info_nonefam(args.suffix, args.filter, args.type)
 
 def get_image_info(familyname, filtertype='r', imagetype='p'):
     '''
@@ -38,7 +68,7 @@ def get_image_info(familyname, filtertype='r', imagetype='p'):
     search_end_date=Time('2017-01-01', scale='utc')     # epoch2=2017+1+1
         
     # Setup output, label columns
-    with open('{}/{}_images.txt'.format(family_dir, familyname), 'w') as outfile:
+    with open('{}/{}_images2.txt'.format(family_dir, familyname), 'w') as outfile:
         outfile.write("{:>10s} {:>10s} {:>10s} {:>16s} {:>16s} {:>16s} {:>12s}\n".format(
             "Object", "Image", "Exp_time", "RA", "DEC", "time", "filter"))
 
@@ -61,7 +91,74 @@ def get_image_info(familyname, filtertype='r', imagetype='p'):
 
         # output the data is previously initiated output file
         if len(obs_in_filter) > 0:
-            with open('{}/{}_images.txt'.format(family_dir, familyname), 'a') as outfile:
+            with open('{}/{}_images2.txt'.format(family_dir, familyname), 'a') as outfile:
+                for line in obs_in_filter:
+                    image_list.append(object_name)
+                    try:
+                        outfile.write("{} {} {} {} {} {} {}\n".format(object_name,
+                            line['Image'], line['Exptime'], line['Object_RA'], line['Object_Dec'],
+                            Time(line['MJD'], format='mjd', scale='utc'), line['Filter']))
+                    except:
+                        print "cannot write to outfile"
+    return image_list
+    
+    
+def get_image_info_nonefam(suffix, filtertype='r', imagetype='p', familyname='none'):
+    '''
+    Query the ssois ephemeris for images of objects in a given family. Then parse through for desired image type, 
+    filter, exposure time, and telescopy instrument
+    '''
+    
+    dir_path_base = '/Users/admin/Desktop/MainBeltComets/getImages/asteroid_families'
+    family_dir = os.path.join(dir_path_base, familyname)
+    if os.path.isdir(family_dir) == False:
+        print "Invalid family name or directory does not exist"
+    
+    family_list = '{}/mba_wo_fam_list_{}.txt'.format(family_dir, suffix)
+    
+    with open(family_list) as infile: 
+        filestr = infile.read()
+    object_list = filestr.split('\n') # array of objects to query
+
+    # TO DO: confirm that the input is the proper format to search for the appropriate ephemeris
+    
+    # From the given input, identify the desired filter and rename appropriately
+    if filtertype.lower().__contains__('r'):
+        filtertype = 'r.MP9601'  # this is the old (standard) r filter for MegaCam
+    if filtertype.lower().__contains__('u'):
+        filtertype = 'u.MP9301'
+
+    # Define time period of image search, basically while MegaCam in operation
+    search_start_date=Time('2013-01-01', scale='utc')   # epoch1=2013+01+01
+    search_end_date=Time('2017-01-01', scale='utc')     # epoch2=2017+1+1
+        
+    # Setup output, label columns
+    with open('{}/mba_images _{}.txt'.format(family_dir, suffix), 'w') as outfile:
+        outfile.write("{:>10s} {:>10s} {:>10s} {:>16s} {:>16s} {:>16s} {:>12s}\n".format(
+            "Object", "Image", "Exp_time", "RA", "DEC", "time", "filter"))
+
+    # Query a specific ephemeris depending on object naming convention. CADC : search = bynameCADC , MPC : search = bynameMPC
+    # Specify search parameters
+        # ephemeris, name, date range, resolve image extension, resolve to x,y, positional uncertainty?
+        # http:// www3.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/cadcbin/ssos/ssosclf.pl?lang=en; object=elst-pizarro; search=bynameMPC; epoch1=2013+01+01; epoch2=2015+1+16; eellipse=; eunits=arcseconds; extres=yes; xyres=yes; format=tsv
+        
+    print "-------------------- \n Searching for images of objects in without family degination of suffix {} from CFHT/Megacam from the MPC ephemeris".format(suffix)
+    print " with filter {} and exposure time of 287, 387, 500 seconds (OSSOS data) \n--------------------".format(filtertype)
+    
+    # ****************************************************************************************************
+    # ****************************************************************************************************
+    image_list = []
+    for object_name in object_list[:len(object_list)-1]:
+        query = Query(object_name, search_start_date=search_start_date, search_end_date=search_end_date)
+
+        # GET/REVIEW THE DATA RETURNED FROM THE SEARCH
+        # Parse the data for Image, Instrument, Filter and create table for each object
+        # Download files with appropriate names? ie object+SSOISfilename ??
+        obs_in_filter = parse_ssois_return(query.get(), object_name, imagetype, camera_filter=filtertype)
+
+        # output the data is previously initiated output file
+        if len(obs_in_filter) > 0:
+            with open('{}/mba_images _{}.txt'.format(family_dir, suffix), 'a') as outfile:
                 for line in obs_in_filter:
                     image_list.append(object_name)
                     try:
@@ -87,7 +184,9 @@ def parse_ssois_return(ssois_return, object_name, imagetype, camera_filter='r.MP
     table_reader.header.splitter.delimiter = '\t'
     table_reader.data.splitter.delimiter = '\t'
     table = table_reader.read(ssois_return)
-
+    
+    
+    
     for row in table:
         # check if a dbimages object exists
         mjd = row['MJD']
@@ -129,32 +228,6 @@ def _skip_missing_data(str_vals, ncols):
         #raise ValueError("not enough columns in table")
         print '  Not enough columns in data table'
     
-def main():
-    '''
-    Input asteroid family, filter type, and image type to query SSOIS
-    '''
-    
-    parser = argparse.ArgumentParser(description='Run SSOIS and return the available images in a particular filter.')
-
-    parser.add_argument("--filter",
-                    action="store",
-                    default='r',
-                    dest="filter",
-                    choices=['r', 'u'],
-                    help="passband: default is r'")
-    parser.add_argument("--family", '-f',
-                        action="store",
-                        default="testfamily/testfamily_family.txt",
-                        help='list of objects to query')
-    parser.add_argument('--type',
-                        default='p',
-                        choices=['o', 'p', 's'], 
-                        help="restrict type of image (unprocessed, reduced, calibrated)")
-
-    args = parser.parse_args()
-    
-    get_image_info(args.family, args.filter, args.type)
-
 
 if __name__ == '__main__':
     main()
