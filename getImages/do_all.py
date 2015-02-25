6dd9bd7e79de8ea7263e17d4f0e18d37918f6c15
kristi <kawebb@uvic.ca>
2015-02-24 19:59:19 -0800

    A lot better

--- getImages/do_all.py
+++ getImages/do_all.py
@@ -6,46 +6,13 @@ from find_family import find_family_members
 from find_family import get_all_families_list
 from get_images import get_image_info
 from get_stamps import get_stamps
-from sep_phot import find_objects_by_phot
+from sep_phot import select_object
 
 from ossos_scripts import storage
 import ossos_scripts.coding
 import ossos_scripts.mpc
 import ossos_scripts.util
 
-
-
-def do_all_things(username, password, familyname, objectname=None, filtertype='r', imagetype='p', radius=0.005, aperture=10.0, thresh=5.0):
-   
-    family_list_path = 'asteroid_families/{}/{}_family.txt'.format(familyname, familyname)
-    if  os.path.exists(family_list_path):
-        print "----- List of objects in family {} exists already -----".format(familyname)
-        with open(family_list_path) as infile:
-            filestr = infile.read()
-            asteroid_list = filestr.split('\n')
-    else:    
-        asteroid_list = find_family_members(familyname)
-
-    image_list_path = 'asteroid_families/{}/{}_images.txt'.format(familyname, familyname)  
-    if  os.path.exists(image_list_path):
-        print "----- List of images in family {} exists already -----".format(familyname)
-        with open(image_list_path) as infile:
-            filestr = infile.read()
-            image_list = filestr.split('\n')
-    else:  
-        image_list = get_image_info(familyname, filtertype, imagetype)       
-    
-    #if objectname == None:
-    #    objectname = image_list[0]
-
-    stamps_dir = 'asteroid_families/{}/{}_stamps'.format(familyname, familyname)
-    if os.path.exists(stamps_dir):
-        print '----- Stamps already exist in VOSpace -----'
-    else:
-        get_stamps(familyname, radius, username, password)
-                
-    #find_objects_by_phot(familyname, objectname, aperture, thresh)    
-              
 def main():
     """
     Input asteroid family name and an asteroid number and get out photometry values
@@ -66,7 +33,7 @@ def main():
                     help="passband: default is r'")
     parser.add_argument("--family", '-f',
                     action="store",
-                    default=None,
+                    default='all',
                     help='list of objects to query')
     parser.add_argument('--type',
                     default='p',
@@ -95,21 +62,66 @@ def main():
     username = raw_input("CADC username: ")
     password = getpass.getpass("CADC password: ")
     
-    allfamily_list_path = 'asteroid_families/all_families.txt'
-    if args.family == None: 
-        if os.path.exists(allfamily_list_path):
-            print "----- List of all family names exists already -----"
-            with open(allfamily_list_path) as infile:
-                filestr = infile.read()
-                families_list = filestr.split('\n')
-        else:    
-            families_list = get_all_families_list()
+    do_all_things(username, password, args.family, args.object, args.filter, args.type, args.radius, args.aperture, args.thresh)
+    
+    '''
+    family_file = 'asteroid_families/{}/{}_family.txt'.format(args.family, args.family)
+    if os.path.exists(family_file):
+        with open(family_file) as infile:
+            for line in infile:
+                do_all_things
+
     
         for familyname in families_list[32:]:
             do_all_things(username, password, familyname, args.object, args.filter, args.type, args.radius, args.aperture, args.thresh)
     
     else:
+        family_list = find_family_members(args.family)
         do_all_things(username, password, args.family, args.object, args.filter, args.type, args.radius, args.aperture, args.thresh)
+    '''
+    
+def do_all_things(username, password, familyname, objectname=None, filtertype='r', imagetype='p', radius=0.005, aperture=10.0, thresh=5.0):
+   
+    family_list_path = 'asteroid_families/{}/{}_family.txt'.format(familyname, familyname) # TEST FILE
+    #print "WARNING: USING A TEST FILE ******************************"
+    if  os.path.exists(family_list_path):
+        print "----- List of objects in family {} exists already -----".format(familyname)
+        with open(family_list_path) as infile:
+            filestr = infile.read()
+            all_object_list = filestr.split('\n')
+    else:    
+        all_object_list = find_family_members(familyname)
+    
+    image_list = []
+    image_list_path = 'asteroid_families/{}/{}_images.txt'.format(familyname, familyname)  
+    if  os.path.exists(image_list_path):
+        print "----- List of images in family {} exists already -----".format(familyname)
+        with open(image_list_path) as infile:
+            filestr = infile.read()
+            fileline = filestr.split('\n')
+            for item in fileline:
+                if len(item.split()) > 0:
+                    image_list.append(item.split()[0])
+    else:  
+        image_list = get_image_info(familyname, filtertype, imagetype) 
+                
+    object_list = []
+    for item in all_object_list:
+        if item in image_list:
+            object_list.append(item)
+
+    '''
+    stamps_dir = 'asteroid_families/{}/{}_stamps'.format(familyname, familyname)
+    if os.path.exists(stamps_dir):
+        print '----- Stamps already exist in VOSpace -----'
+    else:
+        get_stamps(familyname, radius, username, password)
+    '''
+    
+    for objectname in object_list:
+        select_object(familyname, objectname, aperture, thresh)    
+              
+
                         
 if __name__ == '__main__':
     main()                        
