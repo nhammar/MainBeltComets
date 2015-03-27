7accd023fa8c30ddf2462ce17dffbbfb9a529a21
kristi <kawebb@uvic.ca>
2015-03-27 14:46:39 -0700

    Changing method of masking so will preserve while rotating

--- getImages/mbc_detection.py
+++ getImages/mbc_detection.py
@@ -7,30 +7,25 @@ import numpy as np
 import vos
 import argparse
 import os
-from astropy.io import ascii
 import sep
 from astropy.io import fits
+import copy
+
 from scipy.ndimage.interpolation import rotate
+
 import math
 import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.optimize import leastsq
 from shapely.geometry import Polygon
-from collections import Counter
-
-count = Counter()
-
-import sep_phot
-import ossos_scripts.wcs as wcs
 
 import sys
 
 sys.path.append('User/admin/Desktop/OSSOS/MOP/src/ossos-pipeline/ossos')
 from ossos import daophot
 from ossos import storage
-
-sys.path.append('/Users/admin/Desktop/PythonPhot/PythonPhot')
-from PythonPhot import getpsf, aper
+import sep_phot
 
 _VOS_DIR = 'vos:kawebb/postage_stamps/all'
 _DIR_PATH_BASE = os.path.dirname(os.path.abspath(__file__))
@@ -44,10 +39,11 @@ _TARGET = 'OBJECT'
 
 _APERTURE = 10.0
 _THRESHOLD = 3.0
-_BUFFER1 = 15.
-_BUFFER2 = 10.
-_SATURATION_LEVEL = 970
-_SATURATION_THRESHOLD = 0.05  # percent fraction
+_BUFFER1 = 2.5  # used in the shape of the ellipse
+_BUFFER2 = 20.  # used in the intial cutout of the polygon
+_BUFFER3 = 5  # masking center of star psf
+_SATURATION_LEVEL = 3  # sigma? above background
+_SATURATION_THRESHOLD = 3  # maximum allowed pixels above saturation level
 
 client = vos.Client()
 
@@ -75,22 +71,33 @@ def main():
 
     table = pd.read_table('{}/434/phot_output/434_output_test.txt'.format(_DIR_PATH), sep=' ', dtype={'object': object})
 
-    for i in range(0, 1):  # len(input)):
+    for i in range(0, 5):  # len(input)):
 
-        detect_mbc('434', table['object'][i], table['expnum'][i])
+        detect_mbc('434', table['object'][i], table['expnum'][i], i)
 
 
-def detect_mbc(family_name, object_name, expnum):
+def detect_mbc(family_name, object_name, expnum, i):
     """
     Compare psf of asteroid with mean of stars to detect possible comae
     """
     phot_output_file = '{}/{}/phot_output/{}_output_test.txt'.format(_DIR_PATH, family_name, family_name)
     phot_output_table = pd.read_table(phot_output_file, sep=' ', dtype={'object': object})
     asteroid_id = phot_output_table.query('object == "{}" & expnum == "{}"'.format(object_name, expnum))
-    print _BUFFER1, _BUFFER2
+    print 'buffers: ', _BUFFER1, _BUFFER2
     print asteroid_id
 
     header, data = fits_data(object_name, expnum)
+    bkg = get_bkg(data)
+
+    ossos_path = '{}/{}/{}'.format(_OSSOS_PATH_BASE, expnum.strip('p'), header[_CCD])
+    file_fwhm = '{}{}.fwhm'.format(expnum, header[_CCD].split('d')[1])
+    #with storage.open_vos_or_local(file_fwhm) as infile:
+    #    fwhm = float(infile.readline)
+    storage.copy('{}/{}'.format(ossos_path, file_fwhm), '{}/{}'.format(_STAMPS_DIR, file_fwhm))
+    with open('{}/{}'.format(_STAMPS_DIR, file_fwhm)) as infile:
+        fwhm = float(infile.readline())
+
+    print fwhm
 
     '''
     target = header[_TARGET]
@@ -98,10 +105,16 @@ def detect_mbc(family_name, object_name, expnum):
         print '-- NOT in H Block'
         return
     '''
-    # get_star_data(expnum, header)
-    ast_data = get_asteroid_data(asteroid_id, data, header)
 
-    meas_psf(ast_data)
+    print '-- Getting asteroid data'
+    ast_data, x_ast = get_asteroid_data(asteroid_id, data, i, fwhm, bkg)
+    print '-- Getting star data'
+    star_data, x_star = get_star_data(expnum, header, asteroid_id)
+
+    print '-- Mean star fit'
+    meas_psf(star_data, x_star, amp=100, sigma=5.)
+    print '-- Asteroid fit'
+    meas_psf(ast_data, x_ast, amp=100, sigma=10.)
 
 
 def fits_data(object_name, expnum):
@@ -123,6 +136,7 @@ def fits_data(object_name, expnum):
                     data = hdulist[0].data
                     header = hdulist[0].header
 
+                    '''
                     # Measure a spatially variable background of some image data (np array)
                     try:
                         bkg = sep.Background(data)  # , mask=mask, bw=64, bh=64, fw=3, fh=3) # optional parameters
@@ -131,14 +145,15 @@ def fits_data(object_name, expnum):
                         bkg = sep.Background(data)  # , mask=mask, bw=64, bh=64, fw=3, fh=3) # optional parameters
 
                     # Directly subtract the background from the data in place
-                    # bkg.subfrom(data)
+                    bkg.subfrom(data)
+                    '''
 
                     return header, data
 
                     # os.unlink(file_path)
 
 
-def get_star_data(expnum, header):
+def get_star_data(expnum, header, object_data):
     """
     From ossos psf fitted image, calculate line profile
     """
@@ -157,28 +172,19 @@ def get_star_data(expnum, header):
     iraf.daophot(_doprint=0)
     iraf.seepsf(local_file_path, local_psf)
 
-    data = fits.getdata('{}/{}.fits'.format(_STAMPS_DIR, local_psf))
+    with fits.open(local_file_path) as hdulist:
+        data = hdulist[0].data
 
-    return data
+    data_masked = remove_saturated_rows(data, 117)
 
+    th = math.degrees(object_data['theta'].values)
+    data_rot = rotate(data_masked, th)
+    np.savetxt('star_rot.txt', data_rot, fmt='%.0e')
 
-def get_asteroid_data(object_data, data, header):
-    """
-    Calculate psf of asteroid, taking into acount trailing effect
-    """
-
-    # reject any object too bright that will definetly be saturated
-    mag = object_data['mag'].values
-    if mag < 15.5:
-        print '>> Object is too bright for accurate photometry'
-
-    data_cutout = cutout_data(object_data, data, header)
-
-    # sum all the values in each column
+    # sum all the values in each ROW
     totaled = []
-    for row in range(len(data_cutout)):
-
-        sum_col = np.sum(data_cutout[row])
+    for row in range(len(data_rot)):
+        sum_col = np.ma.sum(data_rot[row])
         if sum_col != 0.0:
             totaled.append(sum_col)
 
@@ -190,19 +196,73 @@ def get_asteroid_data(object_data, data, header):
     # normalize the data, perhaps not necessary?
     normed = []
     for item in zeroed:
-        normed.append(item / np.amax(zeroed))
+        normed.append(item / np.amax(zeroed) * 100)
 
-    return normed
+    midpt = int(len(normed) / 2)
+
+    # remove core from psf
+    x = []
+    y = []
+    for i in range(len(normed)):
+        if (i < midpt - _BUFFER3) or (i > midpt + _BUFFER3):
+            x.append(i)
+            y.append(normed[i])
+
+    return y, x
 
 
-def cutout_data(object_data, data, header):
+def get_asteroid_data(object_data, data, i, fwhm, bkg):
+    """
+    Calculate psf of asteroid, taking into acount trailing effect
+    """
+
+    # reject any object too bright that will definetly be saturated
+    mag = object_data['mag'].values
+    if mag < 15.5:
+        print '>> Object is too bright for accurate photometry'
+
+    data_cutout = cutout_data(object_data, data, fwhm, bkg)
+
+    hdu = fits.PrimaryHDU()
+    hdu.data = data_cutout
+    hdu.writeto('cutout_-th_{}_fwhm.fits'.format(i), clobber=True)
+
+    # sum all the values in each ROW
+    totaled = []
+    for row in range(len(data_cutout)):
+        sum_col = np.ma.sum(data_cutout[row])
+        if sum_col != 0.0:
+            totaled.append(sum_col)
+    print totaled
+
+    # shift the baseline to zero
+    zeroed = []
+    for item in totaled:
+        zeroed.append(item - np.amin(totaled))
+
+    # normalize the data, perhaps not necessary?
+    normed = []
+    for item in zeroed:
+        normed.append(item / np.amax(zeroed) * 100)
+
+    x = np.array(range(len(normed)))
+
+    return normed, x
+
+
+def cutout_data(object_data, data, fwhm, bkg):
+    """
+    Cut a square around the polygon of the object, remove saturated rows, rotate so that the ellipse is parallel to the
+    horizontal axis, then mask shape of ellipse around the object
+    """
+
     # make sure there's only one object, otherwise pd.DataFrame.values won't work
     assert len(object_data) == 1
 
     # define parameters of the square to cut out from the polygon around the elongated object
     a = object_data['a'].values[0]
     b = object_data['b'].values[0]
-    th = object_data['theta'].values
+    th = math.degrees(object_data['theta'].values)
     cx = object_data['x'].values
     cy = object_data['y'].values
     a_x = a * math.cos(th)
@@ -213,95 +273,133 @@ def cutout_data(object_data, data, header):
     p2 = (cx + a_x + b_x, cy + a_y + b_y)
     p3 = (cx + a_x - b_x, cy + a_y - b_y)
     p4 = (cx - a_x - b_x, cy - a_y - b_y)
-    polygon = Polygon([p1, p2, p3, p4]).buffer(_BUFFER1)
+    polygon = Polygon([p1, p2, p3, p4]).buffer(_BUFFER2)
     x_min, y_min, x_max, y_max = polygon.bounds
-    data_obj = data[y_min:y_max, x_min:x_max]
-    (pd.DataFrame(data=data_obj)).to_csv('rawdata.txt')
 
-    # data_masked = build_mask(object_data, header)
+    data_obj = np.ones((len(range(int(y_min), int(y_max))), len(range(int(x_min), int(x_max)))))
+    np.copyto(data_obj, data[y_min:y_max, x_min:x_max])
+
+    print 'saturation level, bkg: ', bkg * _SATURATION_LEVEL, bkg
+    data_masked = remove_saturated_rows(data_obj, bkg * _SATURATION_LEVEL)
 
     # rotate the data about the angle of elongation, and cut into a square again
-    data_rot = pd.DataFrame(data=rotate(data_obj, th))
-    print data_rot.shape
+    data_rot = pd.DataFrame(data=rotate(data_masked, th))
+
     rows, cols = data_rot.shape
-    cx2 = int(cols / 2)
+    # cx2 = int(cols / 2)
     cy2 = int(rows / 2)
 
-    y_range = range(cy2 - int(b + _BUFFER1), cy2 + int(b + _BUFFER1))
-    in_ell = []
+    ell_buffer = _BUFFER1 * fwhm
+    print 'buffer, fwhm: ', ell_buffer, fwhm
+
+    y_range = range(cy2 - int(b + ell_buffer), cy2 + int(b + ell_buffer))
 
     mask = np.zeros((rows, cols))
     for y in y_range:
-        x1 = int(float(a + _BUFFER1 + 1) * (1. - (y - cy2) ** 2 / float(b + _BUFFER1 + 1) ** 2) ** 0.5 + cx2)
-        x2 = int(-1 * float(a + _BUFFER1 + 1) * (1. - (y - cy2) ** 2 / float(b + _BUFFER1 + 1) ** 2) ** 0.5 + cx2)
-        for x in range(x2, x1):
-            mask[y - 1][x - 1] = 1
+        x1 = int(cy2 - float(a + ell_buffer + 1) * (1. - (y - cy2) ** 2 / (b + ell_buffer + 1) ** 2) ** 0.5)
+        x2 = int(cy2 + float(a + ell_buffer + 1) * (1. - (y - cy2) ** 2 / (b + ell_buffer + 1) ** 2) ** 0.5)
+        for x in range(x1, x2):
+            mask[y][x] = 1
 
-    data_cutout = np.ma.masked_array(data_rot, mask)
-    np.savetxt('data_cutout.txt', data_cutout, fmt='%.1e')
-    print data_cutout
+    data_cutout = np.multiply(data_rot, mask)
+    # np.savetxt('mask.txt', mask, fmt='%.0e')
+    np.savetxt('data_cutout.txt', data_cutout, fmt='%.0e')
 
-    #hdu = fits.PrimaryHDU()
-    #hdu.data = data_masked
-    #hdu.writeto('cutout.fits', clobber=True)
-
-    '''
-    mid_col = int(len(data_rot.columns) / 2)
-    mid_row = int(len(data_rot) / 2)
-    data_rot_sq = data_rot.iloc[mid_row - int(a + _BUFFER2):mid_row + int(a + _BUFFER2),
-                  mid_col - int(b + _BUFFER2):mid_col + int(b + _BUFFER2)]
-
-    # data_rot_sq.to_csv('data.txt')
-    '''
-
-    return data_cutout
+    return data_cutout.values
 
 
-def build_mask(obj_data, header):
+def remove_saturated_rows(data, sat_level):
     """
     Check for saturation
     """
-    # ccd_satur = header['SATURATE']
-    satur_col = []
-    for row in obj_data:
-        data_row = obj_data[row]
-        sat = data_row.where(data_row > _SATURATION_LEVEL)
-        # sat_np = sat.values
-        sat_arr = sat_np[~np.isnan(sat)]
+    print '-- Checking for saturated pixels'
 
-        if (len(sat_arr) / len(obj_data)) > _SATURATION_THRESHOLD:
-            satur_col.append(row)
+    data_pd = pd.DataFrame(data=data)
 
-    print satur_col
+    satur_row = []
+    mask = []
+    for row in data_pd.index:
+        data_row = data_pd.iloc[row, :]
+        sat = data_row.where(data_row > sat_level)
+        sat_np = sat.values
+        sat_masked = np.ma.masked_array(sat_np, np.isnan(sat_np))
 
-    return obj_data
+        if np.ma.sum(sat_masked) > 3 * sat_level:
+            satur_row.append(row)
+            mask_row = np.ones(len(data_row), dtype=bool)
+        else:
+            mask_row = np.zeros(len(data_row), dtype=bool)
+
+        mask.append(mask_row)
+
+    mask_arr = np.asarray(mask)
+    print 'saturated rows: ', satur_row
+    data_masked = np.ma.masked_array(data, mask_arr)
+
+    # data_pd.drop(data_pd.index[satur_row])
+    # print data_pd
+
+    return data_masked
 
 
-def meas_psf(obj_data):
+def get_bkg(data):
+
+    data2 = np.ones(data.shape)
+    np.copyto(data2, data)
+    try:
+        bkg = (sep.Background(data2)).globalback  # , mask=mask, bw=64, bh=64, fw=3, fh=3) # optional parameters
+
+    except ValueError:
+        data3 = data2.byteswap(True).newbyteorder()
+        bkg = (sep.Background(data3)).globalback  # , mask=mask, bw=64, bh=64, fw=3, fh=3) # optional parameters
+
+    return bkg
+
+
+def meas_psf(data_obj, x, amp, sigma):
     """
     Apply a fit to the point spread function and return parameters of fit
     """
 
     # calculate best parameters for the gaussian fit
-    x = range(len(obj_data))
-    mid_pt = np.argmax(obj_data)
-    fitparams, fitcovariances = curve_fit(gauss, x, obj_data, p0=[1.5, mid_pt, 10.])
-    fitparams2, fitcovariances2 = curve_fit(moffat, x, obj_data, p0=[1.5, mid_pt, 10., 1.])
-    fitparams3, fitcovariances3 = curve_fit(lorentz, x, obj_data, p0=[1.5, mid_pt, 1.])
-    # fitparams4, fitcovariances4 = curve_fit(penny, x, obj_data, p0=[1.5, mid_pt, 7., 10.])
-    print fitparams
-    print fitparams2
-    print fitparams3
-    # print fitparams4
-    gauss_fit = fitparams[0] * np.exp(-(x - fitparams[1]) ** 2 / (2. * fitparams[2] ** 2))  # + fitparams[3]
-    moffat_fit = fitparams2[0] * (1 + (x - fitparams2[1]) ** 2 / fitparams2[2] ** 2) ** (
-        -fitparams2[3])  # + fitparams2[4]
-    lorentz_fit = (fitparams3[0] * np.ones(len(x))) / (
-        1 + (x / (fitparams3[1] * np.ones(len(x)))) ** 2 + x * (fitparams3[2] * np.ones(len(x))))
+    # x = data_obj.index
+    mid_pt = np.argmax(data_obj)
+
+    print x
+    print len(x), len(data_obj)
+
+    fitparams, fitcovariances = curve_fit(gauss, x, data_obj, p0=[amp, mid_pt, sigma, 0.])
+    perr = np.sqrt(np.diag(fitcovariances))
+
+    fitparams2, fitcovariances2 = curve_fit(moffat, x, data_obj, p0=[amp, mid_pt, sigma, 1., 0.])
+    perr2 = np.sqrt(np.diag(fitcovariances2))
+
+    fitparams3, fitcovariances3 = curve_fit(lorentz, x, data_obj, p0=[amp, mid_pt, 1.])
+    perr3 = np.sqrt(np.diag(fitcovariances3))
+
+    # fitparams4, fitcovariances4 = curve_fit(penny, x, data_obj, p0=[1.5, mid_pt, 7., 10.])
+    # perr4 = np.sqrt(np.diag(fitcovariances4))
+
+    print fitparams, perr
+    print fitparams2, perr2
+    print fitparams3, perr3
+    # print fitparams4, perr4
+
+    x2 = range(x[0], x[-1])
+
+    gauss_fit = fitparams[0] * np.exp(-(x2 - fitparams[1]) ** 2 / (2. * fitparams[2] ** 2)) + fitparams[3]
+
+    moffat_fit = fitparams2[0] * (1 + (x2 - fitparams2[1]) ** 2 / fitparams2[2] ** 2) ** (
+        -fitparams2[3]) + fitparams2[4]
+
+    lorentz_fit = (fitparams3[0] * np.ones(len(x2))) / (
+        1 + (x2 / (fitparams3[1] * np.ones(len(x2)))) ** 2 + x2 * (fitparams3[2] * np.ones(len(x2))))
+
     # penny_fit = fitparams4[0] * ((1. - fitparams4[2]) / (1. + (x / fitparams4[1]) ** 2 + fitparams4[2] * np.exp(-0.63 * x ** 2 / fitparams4[1] ** 2 + x * fitparams4[3])))
 
     with sns.axes_style('ticks'):
-        plt.plot(x, obj_data, label='Test data')
+        plt.plot(x, data_obj, label='Object psf')
+        plt.scatter(x, data_obj, label='Object psf')
         plt.plot(x, gauss_fit, label='Gauss fit', ls='--')
         plt.plot(x, moffat_fit, label='Moffat fit', ls=':')
         plt.plot(x, lorentz_fit, label='Lorentz fit', ls='-.')
@@ -311,13 +409,13 @@ def meas_psf(obj_data):
 
 
 def gauss(x, *p):
-    amp, mu, sigma = p
-    return amp * np.exp(-(x - mu) ** 2 / (2. * sigma ** 2))
+    amp, mu, sigma, b = p
+    return amp * np.exp(-(x - mu) ** 2 / (2. * sigma ** 2)) + b
 
 
 def moffat(x, *p):
-    amp, mu, sigma, alpha = p
-    return amp * (1. + (x - mu) ** 2 / sigma ** 2) ** (-alpha)
+    amp, mu, sigma, alpha, b = p
+    return amp * (1. + (x - mu) ** 2 / (2 * sigma ** 2)) ** (-alpha) + b
 
 
 def lorentz(x, *p):
